662d63f2797a2b513f6b58a500732b7632dfa61b
Joel Runnels <runne010@umn.edu>
2018-06-07 12:31:34 -0500

    Pypet Monte Carlo Runs -- Trajectory: `MonteCarloTest_2018_06_07_12h31m34s`, Time: `2018_06_07_12h31m34s`,

--- tests/rangeAttitudeMonteCarloPypet.py
+++ tests/rangeAttitudeMonteCarloPypet.py
@@ -336,7 +336,7 @@ traj.f_add_parameter('runtime', 3600, comment='Length of simulation in seconds')
 #traj.f_add_parameter('pulsarName', 'J0437-4715', comment='Name of the pulsar to run simulation for')
 traj.f_add_parameter('pulsarName', 'B1957+20', comment='Name of the pulsar to run simulation for')
 traj.f_add_parameter('filterTaps', 12, comment='Dimension of correlation vector')
-traj.f_add_parameter('processNoise', 1e-8, comment='Process noise constant added to correlation vector')
+traj.f_add_parameter('processNoise', 1e-12, comment='Process noise constant added to correlation vector')
 traj.f_add_parameter('peakLockThreshold', 0.01, comment='How low the TDOA variance estimate must be in order to reach peak lock.  Unitless; it is defined in terms of the filter dT')
 
 #pulsarList = ['B1957+20']
