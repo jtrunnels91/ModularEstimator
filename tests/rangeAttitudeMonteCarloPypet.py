baac8325b6e6ca444d8a18c876dff8e7c99253e1
Joel Runnels <runne010@umn.edu>
2018-06-07 16:14:23 -0500

    Pypet Monte Carlo Runs -- Trajectory: `MonteCarloTest_2018_06_07_16h14m22s`, Time: `2018_06_07_16h14m22s`,

--- tests/rangeAttitudeMonteCarloPypet.py
+++ tests/rangeAttitudeMonteCarloPypet.py
@@ -363,13 +363,13 @@ traj.f_add_parameter('vVar', np.square(1.0), comment='Variance of velocity measu
 # Attitude information
 traj.f_add_parameter('angularVelocity', [0.0, 0.0, 0.0], comment='Angular velocity of detector in rad/s')
 traj.f_add_parameter('omegaVar', np.square(1e-6), comment='Variance of angular velocity measurement in rad^2/s^2')
-traj.f_add_parameter('initialAttitudeSigma', 0.015 * np.pi/180.0, comment='Variance of initial euler angle uncertainty in radians')
+traj.f_add_parameter('initialAttitudeSigma', 0.0001 * np.pi/180.0, comment='Variance of initial euler angle uncertainty in radians')
 
 traj.f_explore(
     cartesian_product(
         {
             'detectorArea': np.logspace(2, 3, 2),
-            'constantPhaseOffset': np.random.uniform(low=-1.0, high=1.0, size=30)
+            'constantPhaseOffset': np.random.uniform(low=-1.0, high=1.0, size=5)
         }
     )
 )
