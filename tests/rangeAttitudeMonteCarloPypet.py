3c172823224987a1b5ca90feda8da3719fca5943
Joel Runnels <runne010@umn.edu>
2018-06-07 15:56:00 -0500

    Pypet Monte Carlo Runs -- Trajectory: `MonteCarloTest_2018_06_07_15h56m00s`, Time: `2018_06_07_15h56m00s`,

--- tests/rangeAttitudeMonteCarloPypet.py
+++ tests/rangeAttitudeMonteCarloPypet.py
@@ -358,7 +358,7 @@ traj.f_add_parameter('AOAVar', np.square(1e-4), comment='Angle of arrival measur
 traj.f_add_parameter('constantPhaseOffset', np.float64(0), comment='Constant phase delay added to photon arrivals')
 traj.f_add_parameter('orbitPeriod', 100.0/(2*np.pi), comment='Period of orbit in seconds')
 traj.f_add_parameter('orbitAmplitude', 0.0, comment='Amplitude of orbit in km')
-traj.f_add_parameter('vVar', np.square(10.0), comment='Variance of velocity measurement in km^2/s^2')
+traj.f_add_parameter('vVar', np.square(1.0), comment='Variance of velocity measurement in km^2/s^2')
 
 # Attitude information
 traj.f_add_parameter('angularVelocity', [0.0, 0.0, 0.0], comment='Angular velocity of detector in rad/s')
