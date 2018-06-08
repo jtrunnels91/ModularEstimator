b81e75b841084251bc874ffb491bdd4f09d8e0f2
Joel Runnels <runne010@umn.edu>
2018-06-08 08:21:25 -0500

    Pypet Monte Carlo Runs -- Trajectory: `MonteCarloTest_2018_06_08_08h21m25s`, Time: `2018_06_08_08h21m25s`,

--- tests/rangeAttitudeMonteCarloPypet.py
+++ tests/rangeAttitudeMonteCarloPypet.py
@@ -336,20 +336,20 @@ env = Environment(
 traj = env.trajectory
 
 # Monte Carlo simulation parameters
-traj.f_add_parameter('runtime', 101, comment='Length of simulation in seconds')
+traj.f_add_parameter('runtime', 600, comment='Length of simulation in seconds')
 
 
-traj.f_add_parameter('pulsarName', 'J0534+2200', comment='Name of the pulsar to run simulation for')
+#traj.f_add_parameter('pulsarName', 'J0534+2200', comment='Name of the pulsar to run simulation for')
 #traj.f_add_parameter('pulsarName', 'J0437-4715', comment='Name of the pulsar to run simulation for')
 #traj.f_add_parameter('pulsarName', 'B1957+20', comment='Name of the pulsar to run simulation for')
+#traj.f_add_parameter('pulsarName', 'B1937+21', comment='Name of the pulsar to run simulation for')
+traj.f_add_parameter('pulsarName', 'B1821-24', comment='Name of the pulsar to run simulation for')
+
 traj.f_add_parameter('filterTaps', 9, comment='Dimension of correlation vector')
 traj.f_add_parameter('processNoise', 1e-50, comment='Process noise constant added to correlation vector')
 traj.f_add_parameter('scaleProcessNoise', False, comment='Boolean sets whether the process noise is scaled by the detector area.')
 traj.f_add_parameter('peakLockThreshold', 0.01, comment='How low the TDOA variance estimate must be in order to reach peak lock.  Unitless; it is defined in terms of the filter dT')
 
-#pulsarList = ['B1957+20']
-#pulsarList = ['B1937+21']
-#pulsarList=['B1821-24']
 
 # Detector Information
 traj.f_add_parameter('detectorArea', np.float64(100.0), comment='Detector area in cm^2')
@@ -370,7 +370,7 @@ traj.f_add_parameter('initialAttitudeSigma', np.float64(0.1 * np.pi/180.0), comm
 traj.f_explore(
     cartesian_product(
         {
-            'detectorArea': np.logspace(0, 1, 2),
+            'detectorArea': np.logspace(1, 3, 3),
             'constantPhaseOffset': np.random.uniform(low=-1.0, high=1.0, size=10)
         }
     )
