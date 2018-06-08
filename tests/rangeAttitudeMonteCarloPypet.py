c4118dd408da5e0934272fca381cb747ac29a06c
Joel Runnels <runne010@umn.edu>
2018-06-08 09:03:41 -0500

    Pypet Monte Carlo Runs -- Trajectory: `MonteCarloTest_2018_06_08_09h03m41s`, Time: `2018_06_08_09h03m41s`,

--- tests/rangeAttitudeMonteCarloPypet.py
+++ tests/rangeAttitudeMonteCarloPypet.py
@@ -340,11 +340,11 @@ traj.f_add_parameter('runtime', 1000, comment='Length of simulation in seconds')
 
 
 #traj.f_add_parameter('pulsarName', 'J0534+2200', comment='Name of the pulsar to run simulation for')
-#traj.f_add_parameter('pulsarName', 'J0437-4715', comment='Name of the pulsar to run simulation for')
+traj.f_add_parameter('pulsarName', 'J0437-4715', comment='Name of the pulsar to run simulation for')
 #traj.f_add_parameter('pulsarName', 'B1957+20', comment='Name of the pulsar to run simulation for')
 #traj.f_add_parameter('pulsarName', 'B1937+21', comment='Name of the pulsar to run simulation for')
 #traj.f_add_parameter('pulsarName', 'B1821-24', comment='Name of the pulsar to run simulation for')
-traj.f_add_parameter('pulsarName', 'J0030+0451', comment='Name of the pulsar to run simulation for')
+#traj.f_add_parameter('pulsarName', 'J0030+0451', comment='Name of the pulsar to run simulation for')
 
 
 traj.f_add_parameter('filterTaps', 9, comment='Dimension of correlation vector')
