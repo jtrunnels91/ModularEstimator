a7598631ea6ee6a409b1a565a074226a76db158d
Joel Runnels <runne010@umn.edu>
2018-06-11 12:45:36 -0500

    Pypet Monte Carlo Runs -- Trajectory: `MonteCarloTest_2018_06_11_12h45m36s`, Time: `2018_06_11_12h45m36s`,

--- tests/rangeAttitudeMonteCarloPypet.py
+++ tests/rangeAttitudeMonteCarloPypet.py
@@ -239,10 +239,7 @@ def run4DOFSimulation(traj):
         except Exception as inst:
             print(type(inst))    # the exception instance
             print(inst.args)     # arguments stored in .args
-            print(inst)          # __str__ allows args to be printed directly,  but may be overridden in exception subclasses
-            x, y = inst.args     # unpack args
-            print('x =', x)
-            print('y =', y)            
+            print(inst)
             print('Aborting run because of error... restarting.')
             1/0
         
