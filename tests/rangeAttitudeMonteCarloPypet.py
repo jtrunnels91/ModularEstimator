58fb8504455aa14e79af1d00ae23ce4ec8e34011
Joel Runnels <runne010@umn.edu>
2018-06-11 10:20:28 -0500

    Pypet Monte Carlo Runs -- Trajectory: `MonteCarloTest_2018_06_11_10h20m27s`, Time: `2018_06_11_10h20m27s`,

--- tests/rangeAttitudeMonteCarloPypet.py
+++ tests/rangeAttitudeMonteCarloPypet.py
@@ -385,7 +385,7 @@ traj.f_explore(
     cartesian_product(
         {
             'detectorArea': np.logspace(2, 3, 4),
-            'constantPhaseOffset': np.random.uniform(low=-1.0, high=1.0, size=5)
+            'constantPhaseOffset': np.random.uniform(low=-1.0, high=1.0, size=30)
         }
     )
 )
