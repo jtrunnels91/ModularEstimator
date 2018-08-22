6348e749cb56b08d713665b609217a7e231338f6
Joel Runnels <runne010@umn.edu>
2018-08-22 09:50:29 -0500

    Monte carlo of range error vs initial attitude uncertainty, testing again with EKF -- Trajectory: `MonteCarloTest_2018_08_22_09h50m29s`, Time: `2018_08_22_09h50m29s`,

--- tests/rangeAttitudeMonteCarloPypet.py
+++ tests/rangeAttitudeMonteCarloPypet.py
@@ -379,7 +379,7 @@ env = Environment(
 traj = env.trajectory
 
 # Monte Carlo simulation parameters
-traj.f_add_parameter('runtime', 1000, comment='Length of simulation in seconds')
+traj.f_add_parameter('runtime', 150, comment='Length of simulation in seconds')
 
 
 #traj.f_add_parameter('pulsarName', 'J0534+2200', comment='Name of the pulsar to run simulation for')
@@ -427,7 +427,7 @@ traj.f_add_parameter('initialAttitudeSigma', np.float64(1e-9 * np.pi/180.0), com
 traj.f_explore(
     cartesian_product(
         {
-            'initialAttitudeSigma': np.logspace(-4,-4,1) * np.pi/180.0,
+            'initialAttitudeSigma': np.logspace(-4,-2,2) * np.pi/180.0,
             'constantPhaseOffset': np.random.uniform(low=0.0, high=1.0, size=20)
         }
     )
