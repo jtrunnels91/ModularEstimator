cde52a5b29c33ac0f055b8c297b447618837658d
Joel Runnels <runne010@umn.edu>
2019-01-18 17:47:47 -0600

    More work on chandra spacecraft model
    Added a sub-class for dynamics

--- modest/spacecraft/chandra.py
+++ modest/spacecraft/chandra.py
@@ -1,6 +1,8 @@
 from astropy.io import fits
 from pint import UnitRegistry
 import numpy as np
+from scipy.interpolate import interp1d
+
 from .. import utils
 
 class Chandra():
@@ -30,6 +32,17 @@ class Chandra():
         self.ephemData = ephemhdulist[1]
 
         self.tStart = self.detector.getPhotonMeasurement(0)['t']['value'] + tStartOffset
+
+        self.dynamics = ChandraDynamics(
+            ephemerisFile,
+            aspectFile,
+            gyroFile,
+            userData,
+            ureg,
+            self.tStart
+            )
+            
+            
 
 class ChandraDetector():
     def __init__(
@@ -184,6 +197,174 @@ class ChandraDynamics():
             aspectFile,
             gyroFile,
             userData,
-            ureg
+            ureg,
+            tStart
     ):
+        self.tStart=tStart
+        # Import data
+        aspecthdulist = fits.open(aspectFile)
+        aspectData = aspecthdulist[1]
+
+        gyrohdulist = fits.open(gyroFile)
+        gyroData = gyrohdulist[1]
+
+        ephemhdulist = fits.open(ephemFile)
+        ephemData = ephemhdulist[1]
+
+        # Import units from header files for aspect and gyro data, create conversion factors
+        self.recordedRAUnits = ureg(
+            utils.accessPSC.getHeaderInfo('ra', aspectData.header)['unit']
+        )
+        self.recordedDECUnits = ureg(
+            utils.accessPSC.getHeaderInfo('dec', aspectData.header)['unit']
+        )
+        self.recordedRollUnits = ureg(
+            utils.accessPSC.getHeaderInfo('roll', aspectData.header)['unit']
+        )
+        
+        self.gyroUnits = ureg(
+            utils.accessPSC.getHeaderInfo('scratcor', gyroData.header)['unit']
+        )
+
+        self.recordedRaConversionFactor = self.recordedRAUnits.to(ureg('rad')).magnitude
+        self.recordedDecConversionFactor = self.recordedDECUnits.to(ureg('rad')).magnitude
+        self.recordedRollConversionFactor = self.recordedRollUnits.to(ureg('rad')).magnitude
+        self.gyroConversionFactor = self.gyroUnits.to(ureg('rad/s')).magnitude
+        
+
+        # Import position units and create conversion factors
+        self.posX_Units = ureg(utils.accessPSC.getHeaderInfo('X', ephemData.header)['unit'])
+        self.posY_Units = ureg(utils.accessPSC.getHeaderInfo('Y', ephemData.header)['unit'])
+        self.posZ_Units = ureg(utils.accessPSC.getHeaderInfo('Z', ephemData.header)['unit'])
+        
+        self.posXConversionFactor = self.posX_Units.to(ureg('km')).magnitude
+        self.posYConversionFactor = self.posY_Units.to(ureg('km')).magnitude
+        self.posZConversionFactor = self.posZ_Units.to(ureg('km')).magnitude
+
+        # Import velocity units and create conversion factors
+        self.vX_Units = ureg(utils.accessPSC.getHeaderInfo('Vx', ephemData.header)['unit'])
+        self.vY_Units = ureg(utils.accessPSC.getHeaderInfo('Vy', ephemData.header)['unit'])
+        self.vZ_Units = ureg(utils.accessPSC.getHeaderInfo('Vz', ephemData.header)['unit'])
+
+        self.vXConversionFactor = self.vX_Units.to(ureg('km/s')).magnitude
+        self.vYConversionFactor = self.vY_Units.to(ureg('km/s')).magnitude
+        self.vZConversionFactor = self.vZ_Units.to(ureg('km/s')).magnitude
+
+
+        # Get reference MJD and time zero
+        self.MJDREF = ephemData.header['MJDREF']
+        self.timeZero = ephemData.header['TIMEZERO']
+        # self.timeUnits = ureg(utils.accessPSC.getHeaderInfo(
+        #     'time',
+        #     ephemhdulist[0].header
+        # )['unit'])
+
+        self.ephemTimeUnits = ureg(
+            ephemhdulist[0].header['TIMEUNIT']
+        )
+        self.eventTimeConversionFactor = self.ephemTimeUnits.to(ureg('day')).magnitude
+
+
+        # Define a series of interpolation functions to access position,
+        # velocity, angles and angular velocity
+        self.chandraX = interp1d(ephemData.data['time'],ephemData.data['X'])
+        self.chandraY = interp1d(ephemData.data['time'],ephemData.data['Y'])
+        self.chandraZ = interp1d(ephemData.data['time'],ephemData.data['Z'])
+        self.chandraVX = interp1d(ephemData.data['time'],ephemData.data['vX'])
+        self.chandraVY = interp1d(ephemData.data['time'],ephemData.data['vY'])
+        self.chandraVZ = interp1d(ephemData.data['time'],ephemData.data['vZ'])
+
+        self.chandraRoll = interp1d(aspectData.data['time'],aspectData.data['roll'])
+        self.chandraDEC = interp1d(aspectData.data['time'],aspectData.data['dec'])
+        self.chandraRA = interp1d(aspectData.data['time'],aspectData.data['ra'])
+        self.chandraOmegaX = interp1d(gyroData.data['time'],gyroData.data['scratcor'][:,0])
+        self.chandraOmegaY = interp1d(gyroData.data['time'],gyroData.data['scratcor'][:,1])
+        self.chandraOmegaZ = interp1d(gyroData.data['time'],gyroData.data['scratcor'][:,2])
+
+        self.timeObjType = type(self.chandraTimeToTimeScaleObj(self.tStart))
+        return
     
+    def chandraTimeToTimeScaleObj(
+            self,
+            chandraTime
+    ):
+        """
+        See http://cxc.harvard.edu/contrib/arots/time/time_tutorial.html
+        for information
+        """
+        return utils.spacegeometry.timeObj.tt_jd(
+            2400000.5 +
+            self.MJDREF +
+            self.timeZero +
+            (chandraTime * self.eventTimeConversionFactor)
+        )
+    
+    def position(
+            self,
+            t
+    ):
+        # if not isinstance(t,self.timeObjType):
+        tsObj = self.chandraTimeToTimeScaleObj(t)
+                      
+        earthPosition = utils.spacegeometry.earthObj.at(tsObj
+        ).position.km
+
+        chandraPositionX = self.chandraX(t) * self.posXConversionFactor
+        chandraPositionY = self.chandraY(t) * self.posYConversionFactor
+        chandraPositionZ = self.chandraZ(t) * self.posZConversionFactor
+        
+        chandraPostionSSB = (
+            earthPosition +
+            [chandraPositionX, chandraPositionY, chandraPositionZ]
+        )
+        return chandraPostionSSB
+
+    def velocity(
+            self,
+            t
+    ):
+        # if not isinstance(t,self.timeObjType):
+        tsObj = self.chandraTimeToTimeScaleObj(t)
+        earthVelocity = utils.spacegeometry.earthObj.at(
+            tsObj
+        ).velocity.km_per_s
+        
+        chandraVelocityX = self.chandraVX(t) * self.vXConversionFactor
+        chandraVelocityY = self.chandraVY(t) * self.vYConversionFactor
+        chandraVelocityZ = self.chandraVZ(t) * self.vZConversionFactor
+        
+        chandraVelocitySSB = (
+            earthVelocity +
+            [chandraVelocityX, chandraVelocityY, chandraVelocityZ]
+        )
+        return chandraVelocitySSB
+    
+    def attitude(
+            self,
+            t,
+            returnQ=True
+    ):
+        if hasattr(t, '__len__'):
+            attitudeArray = []
+            for i in range(len(t)):
+                attitudeArray.append(self.attitude(t[i],returnQ))
+            return attitudeArray
+        else:
+            roll = self.chandraRoll(t) * self.recordedRollConversionFactor
+            dec = -self.chandraDEC(t) * self.recordedDecConversionFactor
+            ra = self.chandraRA(t) * self.recordedRaConversionFactor
+
+        eulerAngles = [roll, dec, ra]
+        if returnQ:
+            return utils.euler2quaternion(eulerAngles)
+        else:
+            return(eulerAngles)
+
+    def omega(
+            self,
+            t
+    ):
+        omegaX = self.chandraOmegaX(t) * self.gyroConversionFactor
+        omegaY = self.chandraOmegaY(t) * self.gyroConversionFactor
+        omegaZ = self.chandraOmegaZ(t) * self.gyroConversionFactor
+        return [omegaX, omegaY, omegaZ]
